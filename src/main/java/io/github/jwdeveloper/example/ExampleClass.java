171e8d8100e7663819b5e750d596bb2a6106c1cb
JW <jacekwoln@gmail.com>
2023-01-14 10:14:37 +0100

    Test

--- src/main/java/io/github/jwdeveloper/example/ExampleClass.java
+++ src/main/java/io/github/jwdeveloper/example/ExampleClass.java
@@ -1,7 +1,19 @@
 package io.github.jwdeveloper.example;
 
+/**
+ * Returns an Image object that can then be painted on the screen.
+ * argument is a specifier that is relative to the url argument.
+ * <p>
+ * This method always returns immediately, whether or not the
+ * image exists. When this applet attempts to draw the image on
+ * the screen, the data will be loaded. The graphics primitives
+ * that draw the image will incrementally paint on the screen.
+ */
 public class ExampleClass
 {
+    /**
+     * method that do stuff
+     */
     public void show()
     {
         System.out.println("Hello world");
--- src/main/java/io/github/jwdeveloper/example/Main.java
+++ src/main/java/io/github/jwdeveloper/example/Main.java
@@ -1,6 +1,13 @@
 package io.github.jwdeveloper.example;
 
+/**
+ * Main
+ */
 public class Main {
+    /**
+     * Start
+     * @param  args  arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
