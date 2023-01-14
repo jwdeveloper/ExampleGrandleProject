b78083782ba5d92df01524101972ed3449aaac18
JW <jacekwoln@gmail.com>
2023-01-14 07:32:04 +0100

    Test

---
+++ src/main/java/io/github/jwdeveloper/example/ExampleClass.java
@@ -0,0 +1,9 @@
+package io.github.jwdeveloper.example;
+
+public class ExampleClass
+{
+    public void show()
+    {
+        System.out.println("Hello world");
+    }
+}
--- src/main/java/io/github/jwdeveloper/example/Main.java
+++ src/main/java/io/github/jwdeveloper/example/Main.java
@@ -1,4 +1,4 @@
-package io.github.jwdeveloper;
+package io.github.jwdeveloper.example;
 
 public class Main {
     public static void main(String[] args) {
